a3813a390edb3fa1b09ae6ecd5a7b34d6a4d5aaf
Manas Kishore <manaskishore20@gmail.com>
2025-12-14 14:48:26 +0530

    First working code

--- pass_gen.py
+++ pass_gen.py
@@ -1,4 +1,17 @@
 import random 
 
-characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()'
+characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*'
+
+password = ''
+
+number_of_chars = int(input("how long password do you want(4-16): "))
+
+if number_of_chars >=4 and number_of_chars <=16:
+    for i in range(0,number_of_chars):
+
+        num = random.randint(0,len(characters)-1)
+
+        password += characters[num]
+
+print(password)
 
